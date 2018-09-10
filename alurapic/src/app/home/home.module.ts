69b0769cf517005c6c8809ee006e76cbe830be4f
Giuliano Bortoloni <giulianoagria@hotmail.com>
2018-09-10 16:07:39 -0300

    Criando component de SignUp

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { SignInComponent } from './signin/signin.component';
-import { ReactiveFormsModule }  from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { SignUpComponent } from './signup/signup.component';
 
 @NgModule({
-    declarations: [ SignInComponent ],
-    imports: [ 
-        CommonModule, 
+    declarations: [
+        SignInComponent,
+        SignUpComponent
+     ],
+    imports: [
+        CommonModule,
         ReactiveFormsModule,
         VMessageModule,
-        RouterModule
+        RouterModule,
+        FormsModule
     ]
 })
 export class HomeModule { }
