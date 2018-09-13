a7c08bddb3bd9826b0b0b52baa937b39d534db0d
Giuliano Bortoloni <giulianoagria@hotmail.com>
2018-09-13 15:12:57 -0300

    implementando botão de upload

--- alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PhotoService } from '../photo/photo.service';
+import { Router } from '@angular/router';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -10,14 +12,25 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class PhotoFormComponent implements OnInit {
 
   photoForm: FormGroup;
+  file: File;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private photoService: PhotoService, private router: Router) { }
 
   ngOnInit() {
     this.photoForm = this.formBuilder.group({
       file: ['', Validators.required],
-      description: ['', Validators.maxLength(300)]
+      description: ['', Validators.maxLength(300)],
+      allowComments: [true]
     });
+  }
+
+  upload() {
+    const description = this.photoForm.get('description').value;
+    const allowComments = this.photoForm.get('allowComments').value;
+    this.photoService
+      .upload(description, allowComments, this.file)
+      .subscribe(() => this.router.navigate(['']));
+
   }
 
 }
--- alurapic/src/app/photos/photo/photo.component.ts
+++ alurapic/src/app/photos/photo/photo.component.ts
@@ -1,12 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
+const cloud = 'http://localhost:3000/imgs/';
 @Component({
+    // tslint:disable-next-line:component-selector
     selector: 'ap-photo',
-    templateUrl: 'photo.component.html'
+    templateUrl: './photo.component.html'
 })
 export class PhotoComponent {
-    
-    @Input() description='';
-    
-    @Input() url='';
+
+    @Input() description = '';
+    private _url = '';
+
+    @Input() set url(url: string) {
+
+        if (url && url.startsWith('data')) {
+            this._url = url;
+        } else {
+            this._url = cloud + url;
+        }
+    }
+
+    get url() {
+        return this._url;
+    }
 }
