ff22d008970a86d8bf7dad53c55eab354cfb9152
Giuliano Bortoloni <giulianoagria@hotmail.com>
2018-09-13 15:58:56 -0300

    corrigindo erro no upload

--- alurapic/src/app/core/plataform-detector/platform-detector.service.ts
+++ alurapic/src/app/core/plataform-detector/platform-detector.service.ts
@@ -2,7 +2,7 @@ import { Injectable, PLATFORM_ID, Inject } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({ providedIn: 'root'})
-export class PlatformDetectorService { 
+export class PlatformDetectorService {
 
     constructor(@Inject(PLATFORM_ID) private platformId: string) { }
 
